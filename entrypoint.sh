25c219ee16a5a7b28a8bb7e518e8459f20e28431
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-12-24 11:54:19 +0300

    changed entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,11 +21,8 @@ FILENAME=$(echo "${INPUT_FILE}" | xargs -n 1 basename)
 #then
 #    HEADER_RPM_ARCHITECTURE="X-Bintray-Rpm-Architecture: ${INPUT_RPM_ARCHITECTURE}"
 #fi
-pwd
-echo "Uploading file"
-STR ="--silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}""
-echo ${STR}
-curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
-echo "    -> Done."
 
-#curl -T <FILE.EXT> -ukravchenko:<API_KEY> https://api.bintray.com/content/kravchenkoas/bq-tool-my-repo-rpm/bq-rpm/<VERSION_NAME>/<FILE_TARGET_PATH>
+echo "Uploading file"
+#curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
+curl -T ${1} -u ${INPUT_API_USER}:${INPUT_API_KEY} ${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}
+echo "    -> Done."
