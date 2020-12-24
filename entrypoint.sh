aad1447901cbcb37378cc8573c7a1d73d9590995
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-12-24 11:59:21 +0300

    fixed specify full path

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,5 +24,5 @@ FILENAME=$(echo "${INPUT_FILE}" | xargs -n 1 basename)
 
 echo "Uploading file"
 #curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
-curl -T /Users/andreykravchenko/Documents/git cayenne/bootique-tool/${1} -u ${INPUT_API_USER}:${INPUT_API_KEY} ${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}
+curl -T /Users/andreykravchenko/Documents/git\ cayenne/bootique-tool/${1} -u ${INPUT_API_USER}:${INPUT_API_KEY} ${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}
 echo "    -> Done."
