12f4bad3608da007a45a41d999d0d943fa633d13
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-12-24 10:47:27 +0300

    commented out last line

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ set -euf -o pipefail
 
 FILENAME=$(echo "${INPUT_FILE}" | xargs -n 1 basename)
 
-echo ${FILENAME}
-
 #HEADER_RPM_DISTRIBUTION=""
 #if [ "${INPUT_RPM_DISTRIBUTION}" != "" ]
 #then
@@ -28,4 +26,4 @@ echo "Uploading file"
 curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
 echo "    -> Done."
 
-curl -T <FILE.EXT> -ukravchenko:<API_KEY> https://api.bintray.com/content/kravchenkoas/bq-tool-my-repo-rpm/bq-rpm/<VERSION_NAME>/<FILE_TARGET_PATH>
+#curl -T <FILE.EXT> -ukravchenko:<API_KEY> https://api.bintray.com/content/kravchenkoas/bq-tool-my-repo-rpm/bq-rpm/<VERSION_NAME>/<FILE_TARGET_PATH>
