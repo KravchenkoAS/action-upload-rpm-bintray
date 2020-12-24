969f6f18fa8d183798ffc1f0b95bb1a8ee15d84d
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-12-24 10:56:48 +0300

    deleted command ls

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,6 @@ FILENAME=$(echo "${INPUT_FILE}" | xargs -n 1 basename)
 #    HEADER_RPM_ARCHITECTURE="X-Bintray-Rpm-Architecture: ${INPUT_RPM_ARCHITECTURE}"
 #fi
 pwd
-ls
 echo "Uploading file"
 curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
 echo "    -> Done."
