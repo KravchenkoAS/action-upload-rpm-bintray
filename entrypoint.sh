7a984ef068fb1c524340c6fa33f62ec83926db57
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-12-24 11:12:01 +0300

    check string curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,8 @@ FILENAME=$(echo "${INPUT_FILE}" | xargs -n 1 basename)
 #fi
 pwd
 echo "Uploading file"
+STR = --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
+echo STR
 curl --silent --show-error --fail --location --request PUT --upload-file "${1}" --user "${INPUT_API_USER}:${INPUT_API_KEY}" "${INPUT_API_URL}/content/${INPUT_REPOSITORY_USER}/${INPUT_REPOSITORY}/${INPUT_PACKAGE}/${INPUT_VERSION}/${INPUT_UPLOAD_PATH}/${FILENAME};publish=${INPUT_PUBLISH}"
 echo "    -> Done."
 
